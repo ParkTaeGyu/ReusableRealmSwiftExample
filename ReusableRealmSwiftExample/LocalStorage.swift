1bdb90be3908233d41b1afcd78fa993a3fd6caa8
ParkTaeKyu <taekyu2015@gmail.com>
2023-11-28 08:48:13 +0900

    Update func, Error

--- ReusableRealmSwiftExample/LocalStorage.swift
+++ ReusableRealmSwiftExample/LocalStorage.swift
@@ -8,16 +8,22 @@
 import Foundation
 import RealmSwift
 
-struct LocalStorageError: Error {
-    enum ErorCode: Int {
-        case realmObjectNil
-        case entityObjectNil
-    }
+//
+//  LocalStorage.swift
+//  Huskit
+//
+//  Created by Ming on 2023/01/09.
+//
 
-    let errorCode: ErorCode
+import Foundation
+import RealmSwift
+
+enum LocalStorageEror: LocalizedError {
+    case realmObjectNil
+    case entityObjectNil
 
     var description: String {
-        switch errorCode {
+        switch self {
         case .realmObjectNil:
             return " ⚠️ Error Realm Object is nil"
         case .entityObjectNil:
@@ -35,29 +41,40 @@ class LocalStorage {
         return try? Realm()
     }
 
-    func saveObject<T: Object>(_ object: T, update: Realm.UpdatePolicy) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+    /**
+      Realm.UpdatePolicy: 동일한 기본 키를 가진 개체가 이미 존재하는 경우 수행할 작업
+      `error`: Object가 Primary Key를 갖지 않는 경우 사용. Exception Throw
+      `modify`: 기존 객체에서 새 값과 다른 속성만 덮어씀
+      `all`: 기존 객체의 모든 프로퍼티를 변경되지 않은 경우에도 새 값으로 덮어씁니다
+     */
+    func saveObject<T: Object>(_ object: T, update: Realm.UpdatePolicy = .all) throws {
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.add(object, update: update)
         }
     }
 
-    func saveObjects<T: Object>(_ objects: [T], update: Realm.UpdatePolicy) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+    /**
+      Realm.UpdatePolicy: 동일한 기본 키를 가진 개체가 이미 존재하는 경우 수행할 작업
+      `error`: Object가 Primary Key를 갖지 않는 경우 사용. Exception Throw
+      `modify`: 기존 객체에서 새 값과 다른 속성만 덮어씀
+      `all`: 기존 객체의 모든 프로퍼티를 변경되지 않은 경우에도 새 값으로 덮어씁니다
+     */
+    func saveObjects<T: Object>(_ objects: [T], update: Realm.UpdatePolicy = .all) throws {
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.add(objects, update: update)
         }
     }
 
-    func readObject<T: Object>(_ type: T.Type) -> T? {
+    func readObject<T: Object>(_ object: T) -> T? {
         guard let realm = realm else { return nil }
-        let results = realm.objects(type)
-        return results.first
+        return realm.object(ofType: T.self, forPrimaryKey: object.value(forKeyPath: T.primaryKey()!) as AnyObject)
     }
 
-    func readObjects<T: Object>(_ type: T.Type, predicate: NSPredicate? = nil) -> [T]? {
+    func readAllObjectsByType<T: Object>(_ type: T.Type, predicate: NSPredicate? = nil) -> [T]? {
         guard let realm = realm else { return nil }
 
         if let predicate {
@@ -68,15 +85,15 @@ class LocalStorage {
     }
 
     func deleteObject<T: Object>(_ object: T) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.delete(object)
         }
     }
 
-    func deleteObjects<T: Object>(_: T.Type, _ objects: [T]) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+    func deleteObjects<T: Object>(objects: [T]) throws {
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.delete(objects)
@@ -84,7 +101,7 @@ class LocalStorage {
     }
 
     func deleteAllObjectsByType<T: Object>(_: T.Type) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.delete(realm.objects(T.self))
@@ -92,7 +109,7 @@ class LocalStorage {
     }
 
     func deleteAll() throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         try realm.write {
             realm.deleteAll()
@@ -100,21 +117,21 @@ class LocalStorage {
     }
 
     func updateObject<T: Object>(_ object: T) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         let result = realm.object(ofType: T.self, forPrimaryKey: object.value(forKeyPath: T.primaryKey()!) as AnyObject)
 
         if let object = result {
             try? realm.write {
-                realm.add(object, update: .all)
+                realm.add(object)
             }
         } else {
-            throw LocalStorageError(errorCode: .entityObjectNil)
+            throw LocalStorageEror.entityObjectNil
         }
     }
 
     func updateObjects<T: Object>(_ objects: [T]) throws {
-        guard let realm = realm else { throw LocalStorageError(errorCode: .realmObjectNil) }
+        guard let realm = realm else { throw LocalStorageEror.realmObjectNil }
 
         let primaryKeys = objects.map { element -> Any in
             element.value(forKey: T.primaryKey()!) as Any
@@ -123,11 +140,11 @@ class LocalStorage {
         let response = realm.objects(T.self).filter("%@ IN %@", T.primaryKey()!, primaryKeys)
 
         if response.count != objects.count {
-            throw LocalStorageError(errorCode: .entityObjectNil)
+            throw LocalStorageEror.entityObjectNil
         }
 
         try? realm.write {
-            realm.add(objects, update: .all)
+            realm.add(objects)
         }
     }
 }
